88cb4a537d460f82679814423f1a79a8f82e4cf7
Jonathan Lovelace <kingjon3377@gmail.com>
2019-03-22 11:28:35 -0400

    Implement 'adding' feature in menu interface.

--- src/menu/EntityManagementMenu.java
+++ src/menu/EntityManagementMenu.java
@@ -32,18 +32,18 @@ public final class EntityManagementMenu {
 		outStream = out;
 	}
 
-	private String getInputWord(final String prompt) throws IOException {
+	private String getInputLine(final String prompt) throws IOException {
 		outStream.append(prompt);
 		if (!prompt.isEmpty()
 				&& !Character.isWhitespace(prompt.charAt(prompt.length() - 1))) {
 			outStream.append(' ');
 		}
-		return inStream.next();
+		return inStream.nextLine();
 	}
 
 	private void retrieveBook() throws IOException {
 		try { // TODO: allow searching by title, e.g.
-			final String input = getInputWord("ID of book to retrieve (-1 for all):")
+			final String input = getInputLine("ID of book to retrieve (-1 for all):")
 					.trim();
 			final int id = Integer.parseInt(input);
 			if (id < 0) {
@@ -65,7 +65,7 @@ public final class EntityManagementMenu {
 
 	private void retrieveAuthor() throws IOException {
 		try {
-			final String input = getInputWord("ID of author to retrieve (-1 for all):")
+			final String input = getInputLine("ID of author to retrieve (-1 for all):")
 					.trim();
 			final int id = Integer.parseInt(input);
 			if (id < 0) {
@@ -87,7 +87,7 @@ public final class EntityManagementMenu {
 
 	private void retrievePublisher() throws IOException {
 		try {
-			final String input = getInputWord("ID of publisher to retrieve (-1 for all):")
+			final String input = getInputLine("ID of publisher to retrieve (-1 for all):")
 					.trim();
 			final int id = Integer.parseInt(input);
 			if (id < 0) {
@@ -108,7 +108,7 @@ public final class EntityManagementMenu {
 	}
 
 	public void retrieve() throws IOException {
-		final String kind = getInputWord("Kind of entity to retrieve:").trim()
+		final String kind = getInputLine("Kind of entity to retrieve:").trim()
 				.toLowerCase();
 		switch (kind) {
 		case "book": case "b":
@@ -119,6 +119,81 @@ public final class EntityManagementMenu {
 			break;
 		case "publisher": case "p":
 			retrievePublisher();
+			break;
+		default:
+			outStream.append("Entity must be a book, author, or publisher.");
+			outStream.append(System.lineSeparator());
+			break;
+		}
+	}
+
+	private Author createAuthor() throws IOException, NumberFormatException {
+		return service.createAuthor(getInputLine("Name of new author:"));
+	}
+
+	private Publisher createPublisher() throws IOException, NumberFormatException {
+		return service.createPublisher(getInputLine("Name of new publisher:"),
+				getInputLine("Publisher address:"), getInputLine("Publisher phone:"));
+	}
+
+	private void addBook() throws IOException {
+		try {
+			final long authorID = Long.parseLong(
+					getInputLine("ID of author of book (-1 to add new):").trim());
+			final Author author;
+			if (authorID < 0) {
+				author = createAuthor();
+			} else {
+				author = service.getAuthorByID(authorID).get();
+			}
+			final long publisherID = Long.parseLong(
+					getInputLine("ID of publisher of book (-1 to add new):").trim());
+			final Publisher publisher;
+			if (publisherID < 0) {
+				publisher = createPublisher();
+			} else {
+				publisher = service.getPublisherByID(publisherID).get();
+			}
+			service.createBook(getInputLine("Title of new book:"),
+					getInputLine("ISBN of new book:").trim(), author, publisher);
+		} catch (final NumberFormatException except) {
+			outStream.append("ID must be an integer");
+			outStream.append(System.lineSeparator());
+			LOGGER.log(Level.FINER, "Failed to parse integer from input", except);
+		}
+	}
+
+	private void addAuthor() throws IOException {
+		try {
+			createAuthor();
+		} catch (final NumberFormatException except) {
+			outStream.append("ID must be an integer");
+			outStream.append(System.lineSeparator());
+			LOGGER.log(Level.FINER, "Failed to parse integer from input", except);
+		}
+	}
+
+	private void addPublisher() throws IOException {
+		try {
+			createPublisher();
+		} catch (final NumberFormatException except) {
+			outStream.append("ID must be an integer");
+			outStream.append(System.lineSeparator());
+			LOGGER.log(Level.FINER, "Failed to parse integer from input", except);
+		}
+	}
+
+	public void add() throws IOException {
+		final String kind = getInputLine("Kind of entity to add").trim().toLowerCase();
+		switch (kind) {
+		case "book": case "b":
+			addBook();
+			break;
+		case "author": case "a":
+			addAuthor();
+			break;
+		case "publisher": case "p":
+			addPublisher();
 			break;
 		default:
 			outStream.append("Entity must be a book, author, or publisher.");
